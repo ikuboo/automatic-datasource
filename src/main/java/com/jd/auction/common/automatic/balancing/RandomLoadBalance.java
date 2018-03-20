19b0edf4c93d39b3ba92386a7aa08586ad39e43c
yuanchunsen <yuanchunsen@jd.com>
2018-03-20 21:16:11 +0800

    随机策略支持故障数据源的切换

--- src/main/java/com/jd/auction/common/automatic/balancing/RandomLoadBalance.java
+++ src/main/java/com/jd/auction/common/automatic/balancing/RandomLoadBalance.java
@@ -1,7 +1,9 @@
 package com.jd.auction.common.automatic.balancing;
 
 import com.jd.auction.common.automatic.datasouce.NamedDataSource;
+import com.jd.auction.common.automatic.monitor.DataSourceStateJudge;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -11,6 +13,25 @@ import java.util.Random;
 public final class RandomLoadBalance implements LoadBalance {
     @Override
     public NamedDataSource getDataSource(final NamedDataSource masterDataSource,final List<NamedDataSource> slaveDataSources) {
-        return slaveDataSources.get(new Random().nextInt(slaveDataSources.size()));
+
+        List<NamedDataSource> pool = new ArrayList<>(slaveDataSources.size());
+        pool.addAll(slaveDataSources);
+        NamedDataSource select;
+
+        while (true){
+            select = pool.get(new Random().nextInt(slaveDataSources.size()));
+            boolean available = DataSourceStateJudge.isAvailable(select);
+            if(available){
+                break;
+            }else {
+                if(pool.size() <= 1){
+                    break;
+                }else {
+                    pool.remove(select);
+                }
+            }
+        }
+
+        return select;
     }
 }
