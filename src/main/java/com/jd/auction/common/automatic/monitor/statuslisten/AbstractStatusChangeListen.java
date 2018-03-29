a3ef8923289cbe60dfab34bb096621864a262579
yuanchunsen <yuanchunsen@jd.com>
2018-03-29 22:15:05 +0800

    抽象类修改

--- src/main/java/com/jd/auction/common/automatic/monitor/statuslisten/AbstractStatusChangeListen.java
+++ src/main/java/com/jd/auction/common/automatic/monitor/statuslisten/AbstractStatusChangeListen.java
@@ -1,19 +1,8 @@
 package com.jd.auction.common.automatic.monitor.statuslisten;
 
-import com.jd.auction.common.automatic.datasouce.NamedDataSource;
 
 /**
  * @author yuanchunsen@jd.com
  *         2018/3/16.
  */
-public  abstract class AbstractStatusChangeListen implements StatusChangeListen {
-    @Override
-    public void changeToAvailable(final NamedDataSource namedDataSource) {
-
-    }
-
-    @Override
-    public void changeToUnavailable(final NamedDataSource namedDataSource) {
-
-    }
-}
+public  abstract class AbstractStatusChangeListen implements StatusChangeListen {}
